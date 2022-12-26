9e95f3eae5469085d025d32b7bec051c3d296fc9
ibrahimShaikhS <121417063+ibrahimShaikhS@users.noreply.github.com>
2022-12-26 10:48:17 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
+
+with st.container():
+    st.subheader("Hi, I am ibrahim shaikh")
+    st.title("A CS student in FY")
+    st.write("I am passionate about to finding way to use python and to learn new things and to implement that logic to my code")
+    
